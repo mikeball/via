d020e3802b2dcc995a16f91cf86ae39fe3deccfe
Mike Ball <michaelhball@gmail.com>
2014-06-08 21:48:10 -0700

    Removed Content Type
    In order to allow different response formats from handlers, setting
    default content type for responses needs to be handled by calling code.

--- src/taoclj/via.clj
+++ src/taoclj/via.clj
@@ -7,21 +7,10 @@
   (if handler handler (fn [_] {:status status})))
 
 
-(defn set-content-type [response default]
-  (if (get-in response [:headers "Content-Type"])
-    response
-    (assoc-in response [:headers "Content-Type"] default)))
-
-
-
 (defn validate [settings]
   (cond (not (sequential? (:routes settings)))
         (throw (IllegalArgumentException.
-                ":routes setting is required and must be a sequence of routes"))
-
-        (not (string? (:content-type settings)))
-        (throw (IllegalArgumentException.
-                ":content-type setting is required and must be valid http content-type string"))))
+                ":routes setting is required and must be a sequence of routes"))))
 
 
 (defn fn-dispatch
@@ -35,7 +24,6 @@
   (validate settings)
 
   (let [routes           (:routes settings)
-        default-ct       (:content-type settings)
         not-found        (default-handler (:not-found settings) 404)
         not-authorized   (default-handler (:not-authorized settings) 403)
         authenticate     (:authenticate settings)]
@@ -55,8 +43,7 @@
             (assoc :user user)
             (assoc :params (merge (request :params)
                                   (match :path-params)))
-            (handler)
-            (set-content-type default-ct))))))
+            (handler))))))
 
 
 
--- test/taoclj/via_test.clj
+++ test/taoclj/via_test.clj
@@ -4,82 +4,68 @@
 
 
 (deftest require-routes-setting
-  (is (thrown? IllegalArgumentException (via/fn-dispatch {:content-type "ct"}))))
-
-
-(deftest require-content-type-setting
-  (is (thrown? IllegalArgumentException (via/fn-dispatch {:routes '(["/" {}])}))))
+  (is (thrown? IllegalArgumentException (via/fn-dispatch {}))))
 
 
 (deftest return-generic-not-found-if-handler-unspecified
-  (is (= {:status 404 :headers {"Content-Type" "ct"}}
-         ((via/fn-dispatch {:routes         '()
-                            :content-type   "ct"})
+  (is (= {:status 404}
+         ((via/fn-dispatch {:routes '()})
+
           {:uri "/" :request-method :get}))))
 
 
 (deftest return-specified-not-found-if-handler-supplied
-  (is (= {:status 404 :body "nf" :headers {"Content-Type" "ct"}}
-         ((via/fn-dispatch {:routes         '()
-                            :content-type   "ct"
-                            :not-found      (fn [_] {:status 404 :body "nf"})})
+  (is (= {:status 404 :body "nf"}
+         ((via/fn-dispatch {:routes     '()
+                            :not-found  (fn [_] {:status 404 :body "nf"})})
           {:uri "/" :request-method :get}))))
 
 
 (deftest return-generic-not-authorized-if-handler-unspecified
-  (is (= {:status 403 :headers {"Content-Type" "ct"}}
-         ((via/fn-dispatch {:routes         '(["/" {:get ('h :a-role)}])
-                            :content-type   "ct"})
+  (is (= {:status 403}
+         ((via/fn-dispatch {:routes '(["/" {:get ('h :a-role)}])})
           {:uri "/" :request-method :get}))))
 
 
 (deftest return-specified-not-authorized-if-handler-supplied
-  (is (= {:status 403 :body "na" :headers {"Content-Type" "ct"}}
+  (is (= {:status 403 :body "na"}
          ((via/fn-dispatch {:routes         '(["/" {:get ('h :a-role)}])
-                            :content-type   "ct"
                             :not-authorized (fn [_] {:status 403 :body "na"})})
           {:uri "/" :request-method :get}))))
 
 
 (deftest content-type-is-set-on-response
-  (is (= {:status 200 :body "hi" :headers {"Content-Type" "ct"}}
-         ((via/fn-dispatch {:routes         [["/" {:get [(fn [_] {:status 200 :body "hi"}) :public]}]]
-                            :content-type   "ct"})
+  (is (= {:status 200 :body "hi"}
+         ((via/fn-dispatch {:routes [["/" {:get [(fn [_] {:status 200 :body "hi"}) :public]}]]})
           {:uri "/" :request-method :get}))))
 
 
 (deftest generated-dispatch-returns-not-found-on-no-path-match
-   (is (= {:status 404 :headers {"Content-Type" "ct"}}
+   (is (= {:status 404}
           ((via/fn-dispatch {:routes         '(["/a" {}])
-                             :content-type   "ct"
                              :not-found      (fn [r] {:status 404})})
 
            {:uri "/b" :request-method :get}))))
 
 
 (deftest generated-dispatch-returns-method-not-allowed-on-no-method-match
-   (is (= {:status 405 :headers {"Content-Type" "ct"}}
-          ((via/fn-dispatch {:routes         '(["/a" {}])
-                             :content-type   "ct"})
+   (is (= {:status 405}
+          ((via/fn-dispatch {:routes         '(["/a" {}])})
            {:uri "/a" :request-method :get}))))
 
 
 (deftest generated-dispatch-enforces-authorization
-   (is (= {:status 403 :headers {"Content-Type" "ct"} :body "na"}
+   (is (= {:status 403 :body "na"}
           ((via/fn-dispatch {:routes         '(["/a" {:get ('h :a-role)}])
-                             :content-type   "ct"
                              :not-authorized (fn [_] {:status 403 :body "na"})})
            {:uri "/a"
             :request-method :get
             :user nil}))))
 
 
- (deftest generated-dispatch-allows-valid-roles
-   (is (= {:status 200
-           :headers {"Content-Type" "ct"}
-           :body "ok"}
+(deftest generated-dispatch-allows-valid-roles
+   (is (= {:status 200 :body "ok"}
           ((via/fn-dispatch {:routes       [["/a" {:get [(fn [r] {:status 200 :body "ok"}) :a-role]}]]
-                             :content-type "ct"
                              :authenticate (fn [_] {:roles [:a-role]})})
            {:uri "/a"
             :request-method :get}))))
@@ -89,9 +75,7 @@
    (is (= "1"
           (:body ((via/fn-dispatch {:routes [["/a/:id"
                                               {:get [(fn [r] {:status 200 :body (-> r :params :id)})
-                                                     :public]}]]
-
-                                    :content-type "ct"})
+                                                     :public]}]]})
                   {:uri "/a/1"
                    :request-method :get})))))
 
