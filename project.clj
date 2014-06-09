5b8fb0cf580de280868cee13de21f51a15a680e5
Mike Ball <michaelhball@gmail.com>
2014-06-08 19:48:21 -0700

    Removed unnecessary settings and dependencies

--- project.clj
+++ project.clj
@@ -7,10 +7,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clout "1.2.0"]
-                 [ring/ring-core "1.3.0"]]
-
-  ; unknown what we're using ring-devel for
-  ;:profiles {:dev {:dependencies [[ring/ring-devel "1.3.0"]]}}
-
-)
+                 [clout "1.2.0"]])
