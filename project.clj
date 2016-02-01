d472758befd9dbba1491aafa95ddd6d1b963890a
Mike Ball <michaelhball@gmail.com>
2016-01-31 18:16:56 -0800

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.taoclj/via "0.0.1-SNAPSHOT"
+(defproject org.taoclj/via "0.1.0"
   :description "A ring routing library with integrated authorization"
 
   :url "http://github.com/mikeball/via"
-  
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clout "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clout "2.1.2"]])
