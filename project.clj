bcfcaa40d04ed22ba14d5abfb9dc8516fb7c4076
mikeball <michaelhball@gmail.com>
2019-06-24 22:38:59 -0700

    upgrade clojure

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clout "2.2.1"]])
